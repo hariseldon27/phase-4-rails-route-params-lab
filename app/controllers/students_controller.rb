90c35756b9fec7f28eff3eb3ab8134f9a3da93fa
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-12 09:13:17 -0800

    commit tree b4d3b15a720e24aac71a2e712d6bf573eb2c306e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,13 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  def search
+    result = Student.where(["first_name = ? or last_name = ?", params[:q], params[:q] ])
+    render json: result
+  end
 
 end
